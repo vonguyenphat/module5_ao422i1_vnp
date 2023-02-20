7680fae7eb8656585504cb45a6e01081499580fb
Võ Nguyễn Phát <vonguyenphat012@gmail.com>
2023-02-20 18:18:39 +0700

    up

---
+++ ss3_angular_overview/bai_tap/calculator/angular-calculator-app/src/app/calculator/calculator.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator',
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.css']
+})
+export class CalculatorComponent implements OnInit {
+  firstValue: number;
+  display: string = '0';
+  action: string;
+  constructor() { }
+  ngOnInit(): void {
+  }
+  dis(val: string){
+      this.display = val;
+  }
+  oper(action){
+    this.action = action;
+    this.firstValue = parseFloat(this.display);
+    this.display ='';
+  }
+  calculate(){
+    const a = this.firstValue;
+    const b = parseFloat(this.display);
+    let result: number;
+    if (this.action === '*'){
+      result = a*b;
+    }
+    if (this.action === '/'){
+      result = a/b;
+    }
+       if (this.action === '+'){
+      result = a+b;
+    }
+       if (this.action === '-'){
+      result = a-b;
+    }
+       this.firstValue = result;
+       this.display = result.toString();
+  }
+  clr(){
+    this.firstValue = 0;
+    this.display = '0';
+  }
+}
